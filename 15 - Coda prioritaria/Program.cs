a4002f5fab917bec2964113a91da2a899fb8998e
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2024-02-20 13:11:21 +0100

    metodo Aggiunta e isEmpty

--- 15 - Coda prioritaria/Program.cs
+++ 15 - Coda prioritaria/Program.cs
@@ -10,9 +10,27 @@ namespace _15___Coda_prioritaria
     
     internal class Program
     {
+        class CodaPrioritaria
+        {
+            // attributi
+            ArrayList arrayList;
+            // costruttore
+            public CodaPrioritaria()
+            {
+               arrayList = new ArrayList();
+            }
+
+            // metodi
+            public void Aggiunta(Object obj)
+            {
+                arrayList.Add(obj);
+            }
+        }
         static void Main(string[] args)
         {
-
+            CodaPrioritaria codaPrioritaria = new CodaPrioritaria();
+            Console.WriteLine("Inserire elemento: ");
+            string stringa = Console.ReadLine();
         }
     }
 }
